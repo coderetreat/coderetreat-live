ac798cacd51f5fd3026376c47e264cb79f03488b
coreyhaines <coreyhaines@gmail.com>
2012-12-02 17:01:21 -0600

    Presenter namespace for collection begins

--- app/coderetreat_live/coderetreats.rb
+++ app/coderetreat_live/coderetreats.rb
@@ -10,22 +10,25 @@ module CoderetreatLive
     end
 
     def self.for_collection(coderetreats)
-      Collection.new(coderetreats)
+      Presenters::Collection.new(coderetreats)
     end
-    Collection = Struct.new(:coderetreats) do
-      def empty?
-        self.coderetreats.empty?
-      end
 
-      [:not_started,
-        :in_session].each do |status|
-        define_method status do |&block|
-          self.by_status status, &block
+    module Presenters
+      Collection = Struct.new(:coderetreats) do
+        def empty?
+          self.coderetreats.empty?
         end
-      end
 
-      def by_status(status, &block)
-        self.coderetreats.select{|coderetreat| coderetreat.status.to_sym == status.to_sym}.each(&block)
+        [:not_started,
+          :in_session].each do |status|
+          define_method status do |&block|
+          self.by_status status, &block
+          end
+          end
+
+        def by_status(status, &block)
+          self.coderetreats.select{|coderetreat| coderetreat.status.to_sym == status.to_sym}.each(&block)
+        end
       end
     end
   end
