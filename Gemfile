2f47cae5375fcf6a4542dc49b32ff44f6550908d
coreyhaines <coreyhaines@gmail.com>
2012-12-02 15:20:26 -0600

    The first step to world dominiation is making a commitment

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'https://rubygems.org'
+ruby '1.9.3'
+
+gem 'rails', '3.2.9'
+gem 'sqlite3'
+
+gem 'jquery-rails'
+
+gem 'sass'
+gem 'haml'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+  gem 'database_cleaner'
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoderetreatLive::Application.config.secret_token = '87f48957b171bd6155fa6d47c709b9cdbacc00ea32b5cf28a53f5784f9160bf2be033700562bb3a924d9a8e798f16ac0b4c334853480544c7b86394ea5e93387'
