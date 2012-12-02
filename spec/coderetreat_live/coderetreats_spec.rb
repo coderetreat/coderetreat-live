8552bd8afeedccd9dc6f9d8fa4c006ae90846a31
coreyhaines <coreyhaines@gmail.com>
2012-12-02 16:45:56 -0600

    Refactor coderetreats collection specs for easier new writing

--- spec/coderetreat_live/coderetreats_spec.rb
+++ spec/coderetreat_live/coderetreats_spec.rb
@@ -1,21 +1,27 @@
 require "coderetreat_live/coderetreats"
 
 describe "Coderetreat list" do
+  let(:collection) { [] }
+  let(:coderetreats) { CoderetreatLive::Coderetreats.for_collection(collection) }
+
   describe "#empty?" do
     it "returns if passed in collection is empty" do
-      CoderetreatLive::Coderetreats.for_collection([]).should be_empty
-      CoderetreatLive::Coderetreats.for_collection([stub]).should_not be_empty
+      coderetreats.should be_empty
+      collection << stub
+      coderetreats.should_not be_empty
     end
   end
 
   describe "filtering the collection passed in by status" do
+    let(:coderetreat1) { stub(:status => "") }
+    let(:coderetreat2) { stub(:status => "") }
+    before do
+      collection << coderetreat1 << coderetreat2
+    end
     describe "#not_started" do
       it "returns coderetreats that are in not_started status" do
-        coderetreat1 = stub(:status => "not_started")
-        coderetreat2 = stub(:status => "in_session")
-
-        coderetreats = CoderetreatLive::Coderetreats.for_collection([coderetreat1, coderetreat2])
-
+        coderetreat1.stub(:status => "not_started")
+        coderetreat2.stub(:status => "in_session")
         coderetreats.not_started.should =~ [coderetreat1]
       end
     end
