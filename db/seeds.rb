549a4d0ff9ae9bcbd4c1cc9794fdda0bee801031
coreyhaines <coreyhaines@gmail.com>
2012-12-03 13:09:18 -0600

    Seed file now uses STATES from coderetreat state machine

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
+require 'coderetreats/state_machine'
+
 Coderetreat.destroy_all
 
-states = [:in_session, :not_started]
-10.times do |index|
+states = CoderetreatLive::Coderetreats::StateMachine::STATES
+50.times do |index|
   Coderetreat.create status: states.sample, location: "City #{index}", date: DateTime.now.to_date
 end
