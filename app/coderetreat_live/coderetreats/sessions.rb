6f78fdeada5c4e25879542f4e0c442bbbd204d07
coreyhaines <coreyhaines@gmail.com>
2012-12-05 07:34:05 -0800

    Sessions manager tells coderetreat to start a new session

--- app/coderetreat_live/coderetreats/sessions.rb
+++ app/coderetreat_live/coderetreats/sessions.rb
@@ -4,6 +4,7 @@ module CoderetreatLive
     module Sessions
       def self.start_new_session_for(coderetreat_token, session_details)
         coderetreat = Coderetreats.update_status(coderetreat_token, "in_session")
+        coderetreat.start_new_session(session_details)
       end
     end
   end
--- spec/coderetreat_live/coderetreats/sessions_spec.rb
+++ spec/coderetreat_live/coderetreats/sessions_spec.rb
@@ -1,10 +1,26 @@
 require 'coderetreats/sessions'
 
 describe "Starting a new session" do
-  it "updates the status of the coderetreat to 'in_session'" do
-    coderetreat = stub
-    CoderetreatLive::Coderetreats.should_receive(:update_status).with("token", "in_session") { coderetreat }
+  describe ".start_new_session_for" do
+    let(:coderetreat) { stub }
+    let(:session) { stub }
+    before do
+      CoderetreatLive::Coderetreats.stub(:update_status) { coderetreat }
+      coderetreat.stub(:start_new_session) { session }
+    end
+    it "updates the status of the coderetreat to 'in_session'" do
+      CoderetreatLive::Coderetreats.should_receive(:update_status).with("token", "in_session") { coderetreat }
 
-    CoderetreatLive::Coderetreats::Sessions.start_new_session_for("token", {"constraints" => "irrelevant"})
+      CoderetreatLive::Coderetreats::Sessions.start_new_session_for("token", {"constraints" => "irrelevant"})
+    end
+
+    it "tells the coderetreat to start a new session" do
+      coderetreat.should_receive(:start_new_session).with({"constraints" => "irrelevant"})
+      CoderetreatLive::Coderetreats::Sessions.start_new_session_for("token", {"constraints" => "irrelevant"})
+    end
+
+    it "returns the session that was started" do
+      CoderetreatLive::Coderetreats::Sessions.start_new_session_for("token", stub).should == session
+    end
   end
 end
