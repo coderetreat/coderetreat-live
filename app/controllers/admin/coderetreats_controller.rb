de2801edd5b842e566cf52d094eefe125bc0d1d2
coreyhaines <coreyhaines@gmail.com>
2012-12-04 19:58:18 -0500

    Redirect after updating status back to manage page

--- app/controllers/admin/coderetreats_controller.rb
+++ app/controllers/admin/coderetreats_controller.rb
@@ -3,4 +3,8 @@ class Admin::CoderetreatsController < ApplicationController
   def manage
     @coderetreat = CoderetreatLive::Coderetreats.for_admin_token(params[:id])
   end
+
+  def update_status
+    redirect_to manage_coderetreat_url(id: params[:id])
+  end
 end
