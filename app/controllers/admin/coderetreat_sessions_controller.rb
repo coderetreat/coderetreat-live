b4d617fcddb4fe497443ee14a53ca60614a37821
coreyhaines <coreyhaines@gmail.com>
2012-12-07 15:58:59 +1100

    Structure (controller, route) for updating coderetreat session

--- app/controllers/admin/coderetreat_sessions_controller.rb
+++ app/controllers/admin/coderetreat_sessions_controller.rb
@@ -4,6 +4,10 @@ class Admin::CoderetreatSessionsController < ApplicationController
     @session, @coderetreat = CoderetreatLive::Coderetreats::Sessions.for_coderetreat(params[:coderetreat_id], params[:id])
   end
 
+  def update
+    redirect_to edit_coderetreat_status_url(coderetreat_id: params[:coderetreat_id])
+  end
+
   def create
     CoderetreatLive::Coderetreats::Sessions.start_new_session_for(params[:coderetreat_id], params[:coderetreat_session])
     redirect_to edit_coderetreat_status_url(coderetreat_id: params[:coderetreat_id])
