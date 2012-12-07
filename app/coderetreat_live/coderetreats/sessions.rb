2f1f945ad6d4671e838dbd97ffe207b7cfe92838
coreyhaines <coreyhaines@gmail.com>
2012-12-07 14:57:50 +1100

    Coderetreats::Sessions.remove_session_from tells coderetreat to do it

--- app/coderetreat_live/coderetreats/sessions.rb
+++ app/coderetreat_live/coderetreats/sessions.rb
@@ -6,6 +6,11 @@ module CoderetreatLive
         coderetreat = Coderetreats.update_status(coderetreat_token, Coderetreats::StateMachine.in_session_status_string)
         coderetreat.start_new_session(session_details)
       end
+
+      def self.remove_session_from(coderetreat_token, session_id)
+        coderetreat = Coderetreats.for_admin_token(coderetreat_token)
+        coderetreat.remove_session(session_id)
+      end
     end
   end
 end
--- spec/coderetreat_live/coderetreats/sessions_spec.rb
+++ spec/coderetreat_live/coderetreats/sessions_spec.rb
@@ -1,8 +1,8 @@
 require 'coderetreats/sessions'
 
 describe "Starting a new session" do
+  let(:coderetreat) { stub }
   describe ".start_new_session_for" do
-    let(:coderetreat) { stub }
     let(:session) { stub }
     before do
       CoderetreatLive::Coderetreats.stub(:update_status) { coderetreat }
@@ -21,6 +21,16 @@ describe "Starting a new session" do
 
     it "returns the session that was started" do
       CoderetreatLive::Coderetreats::Sessions.start_new_session_for("token", stub).should == session
+    end
+  end
+
+  describe ".remove_session_from" do
+    it "tells coderetreat to remove the session given by the id" do
+      CoderetreatLive::Coderetreats.stub(:for_admin_token).with("token") { coderetreat }
+      coderetreat.should_receive(:remove_session).with("5")
+
+
+      CoderetreatLive::Coderetreats::Sessions.remove_session_from("token", "5")
     end
   end
 end
