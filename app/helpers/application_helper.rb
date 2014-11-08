67f822fdcf3cc045424427c9cb00b0fb1b1eddf4
Grayson Wright <wright.grayson@gmail.com>
2014-11-07 19:37:46 -0800

    Add a small map next to each coderetreat

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def google_map_image_for(city)
+    "https://maps.googleapis.com/maps/api/staticmap?markers=#{city}&size=100x100&zoom=1" +
+    "&style=feature:all|element:labels|visibility:off"
+  end
 end
