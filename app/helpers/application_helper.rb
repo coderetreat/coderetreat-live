a3d107e1e016624ed15cde55812743f9a5aa7f09
Grayson Wright <wright.grayson@gmail.com>
2014-11-07 19:53:55 -0800

    Steal sponsors section from GDCR landing page
    See https://github.com/coreyhaines/GDCR

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,28 @@ module ApplicationHelper
     "https://maps.googleapis.com/maps/api/staticmap?markers=#{city}&size=100x100&zoom=1" +
     "&style=feature:all|element:labels|visibility:off"
   end
+
+  def globe_sponsors
+    [
+      Sponsor.new('Github', 'https://github.com'),
+    ]
+  end
+
+  def continent_sponsors
+    [
+      Sponsor.new('DNSimple', 'https://www.dnsimple.com/'),
+    ]
+  end
+
+  class Sponsor
+    def initialize name, url
+      @name, @url = name, url
+    end
+
+    def logo_path
+      "http://globalday.coderetreat.org/images/sponsors/#{name.downcase}_logo.png"
+    end
+
+    attr_reader :name, :url
+  end
 end
