3af377a09a3ebeb25661b4cfbaa663a1b8ae94ad
Grayson Wright <wright.grayson@gmail.com>
2014-11-09 01:28:58 -0800

    Use the cached images in the Live page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 module ApplicationHelper
   def google_map_image_for(city)
-    "https://maps.googleapis.com/maps/api/staticmap?markers=#{city}&size=100x100&zoom=1" +
-    "&style=feature:all|element:labels|visibility:off"
+    "/map_images/#{city.gsub(" ", "+")}"
   end
 
   def all_sponsors
